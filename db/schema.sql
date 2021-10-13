45a022aab9751fbfae00c4189edc2fceb17053f1
James Capadocia <james.capadocia@gmail.com>
2021-10-13 14:00:58 -0500

    Coded schema.sql to create appropriate tables for project. Will need to code seeds.sql to confirm tables will generate appropriately.

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,33 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
 
+USE employees_db;
 
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+)
 
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT,
+  FOREIGN KEY (department)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+)
 
-
--- department
-
-id: INT PRIMARY KEY
-name: VARCHAR(30) to hold department name
-
-
-
--- role
-
-id: INT PRIMARY KEY
-title: VARCHAR(30) to hold role title
-salary: DECIMAL to hold role salary
-department_id: INT to hold reference to department role belongs to
-
-
-
--- employee
-
-id: INT PRIMARY KEY
-first_name: VARCHAR(30) to hold employee first name
-last_name: VARCHAR(30) to hold employee last name
-role_id: INT to hold reference to employee role
-manager_id: INT to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  FOREIGN KEY (role)
+  REFERENCES role(id)
+  ON DELETE SET NULL
+  manager_id INT,
+  FOREIGN KEY (manager)
+  REFERENCES employee(id)
+  ON DELETE SET NULL
+)
