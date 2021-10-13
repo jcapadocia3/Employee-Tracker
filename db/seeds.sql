57ad241eb44a31968e4082c9eb7d2590fbc02b23
James Capadocia <james.capadocia@gmail.com>
2021-10-13 14:46:23 -0500

    Coded seeds.sql to test tables created from schema.sql. While the 'department' table is created, it is having an issue creating the 'role' and 'employee' tables.

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (name)
+VALUES ("Sales"),
+       ("Engineering"),
+       ("Finance"),
+       ("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Team Lead", 45000, 1),
+       ("Salesperson", 35000, 1),
+       ("Lead Engineer", 98000, 2),
+       ("Software Engineer", 80000, 2),
+       ("Account Manager", 70000, 3),
+       ("Accountant", 59000, 3),
+       ("Legal Team Lead", 90000, 4),
+       ("Lawyer", 75000, 4);
+
+-- INSERT INTO employee (first_name, last_name, role_id)
+-- VALUES ("Cheryl", "DeGuzman", 1),
+--        ("Mary", "LittleLamb", 2),
+--        ("James", "Capadocia", 3),
+--        ("Bat", "Man", 4),
+--        ("Chuck", "Norris", 5),
+--        ("Hokey", "Pokey", 6),
+--        ("Tom", "Jerry", 7),
+--        ("Tom", "Sawyer", 8);
