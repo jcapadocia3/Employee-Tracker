3e8c7b9c9688638dcb0163596dc5807461a08570
James Capadocia <james.capadocia@gmail.com>
2021-10-13 22:04:42 -0500

    Started '.then' code to write first conditional statement and corresponding function to test based on user choice from prompts. Keep receiving error that something syntactically is incorrect.

--- server.js
+++ server.js
@@ -13,24 +13,47 @@ const db = mysql.createConnection(
 );
 
 const userPrompts = () => {
+
   console.log(`--------------------------`)
   console.log(`What would you like to do?`)
   console.log(`--------------------------`)
-  inquirer.prompt (
-    [{
-      type: 'list',
-      name: 'choices',
-      choices: [
-        'View all departments',
-        'View all roles',
-        'View all employees',
-        'Add a department',
-        'Add a role',
-        'Add an employee',
-        'Update an employee role'
-      ]
-    }]
-  );
+
+  return inquirer
+    .prompt ([
+      {
+        type: 'list',
+        name: 'choices',
+        choices: [
+          'View all departments',
+          'View all roles',
+          'View all employees',
+          'Add a department',
+          'Add a role',
+          'Add an employee',
+          'Update an employee role'
+        ]
+      }
+    ]);
+  
+
+    .then((userRes) => {
+
+      const { choices } = userRes;
+
+      if (choices === 'View all departments') {
+        showDepartments = () => {
+          console.log('Showing all departments...\n');
+          const sql = `SELECT department.id AS id, department.name AS department FROM department`; 
+        
+          connection.promise().query(sql, (err, rows) => {
+            if (err) throw err;
+            console.table(rows);
+            userPrompts();
+          })
+        }
+      }
+    })
+  
 };
 
 userPrompts();
