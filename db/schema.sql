13e318aa5277a0dc8569d28400ed110ae505ab39
James Capadocia <james.capadocia@gmail.com>
2021-10-17 23:54:57 -0500

    Commented out 'schema.sql' and 'seeds.sql' files.

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,17 @@
+-- Creates database
 DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 
+-- Uses database created
 USE employees_db;
 
+-- Create 'department table' and determine data parameters
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
 
+-- Create 'role table' and determine data parameters
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
@@ -18,6 +22,7 @@ CREATE TABLE role (
   ON DELETE SET NULL
 );
 
+-- Create 'employee table' and determine data parameters
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,13 @@
+-- Pre-entered data to be stored in database and shown upon 'npm start' when user chooses to show existing data via inquirer prompts
+
+-- Pre-entered data in 'department table'
 INSERT INTO department (name)
 VALUES ("Sales"),
        ("Engineering"),
        ("Finance"),
        ("Legal");
 
+-- Pre-entered data in 'role table'
 INSERT INTO role (title, salary, department)
 VALUES ("Sales Team Lead", 45000, 1),
        ("Salesperson", 35000, 1),
@@ -14,6 +18,7 @@ VALUES ("Sales Team Lead", 45000, 1),
        ("Legal Team Lead", 90000, 4),
        ("Lawyer", 75000, 4);
 
+-- Pre-entered data in 'employee table'
 INSERT INTO employee (first_name, last_name, role, manager)
 VALUES ("Cheryl", "DeGuzman", 1, null),
        ("Mary", "LittleLamb", 2, 1),
